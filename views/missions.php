83d5233a4eee5a0e4d63623dbd1181623a158fec
jessicaqiu <jessicaelaineqiu@gmail.com>
2019-01-28 13:11:00 -0800

    Fixed CSS

--- views/missions.php
+++ views/missions.php
@@ -12,37 +12,37 @@
 	</div><!--// missions-heading-->
 
 	<div id="wishlist">
-		<div class="wish">
+		<div class="wish-item">
 			<div>
 				<a href="#"><span class="fas fa-minus-circle"></span></a>
 				<p>"I want to meet Batman."</p>
 			</div>
 			<p>St. Mary's Hospital</p>
-			<a href="#" class="fulfill">Fulfill</a>
+			<a href="#" class="fulfillBtn">Fulfill</a>
 		</div><!--//wish-->
-		<div class="wish">
+		<div class="wish-item">
 			<div>
 				<a href="#"><span class="fas fa-minus-circle"></span></a>
 				<p>"I want a toy truck."</p>
 			</div>
 			<p>Mount George Hospital</p>
-			<a href="#" class="fulfill">Fulfill</a>
+			<a href="#" class="fulfillBtn">Fulfill</a>
 		</div><!--//wish-->
-		<div class="wish">
+		<div class="wish-item">
 			<div>
 				<a href="#"><span class="fas fa-minus-circle"></span></a>
 				<p>"I wish to see the Avengers movie."</p>
 			</div>
 			<p>St. Joseph Children's Hospital</p>
-			<a href="#" class="fulfill">Fulfill</a>
+			<a href="#" class="fulfillBtn">Fulfill</a>
 		</div><!--//wish-->
-		<div class="wish">
+		<div class="wish-item">
 			<div>
 				<a href="#"><span class="fas fa-minus-circle"></span></a>
 				<p>"I would like to make slime."</p>
 			</div>
 			<p>St. Andrew's Children's Hospital</p>
-			<a href="#" class="fulfill">Fulfill</a>
+			<a href="#" class="fulfillBtn">Fulfill</a>
 		</div><!--//wish-->
 	</div><!--//wishlist-->
 
