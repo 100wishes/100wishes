cca70e596bc85bdc1507e9045f2945409950b664
jessicaqiu <jessicaelaineqiu@gmail.com>
2019-02-06 15:44:00 -0800

    Create Account page HTML CSS

--- views/signup.php
+++ views/signup.php
@@ -5,22 +5,34 @@
 	</div><!-- //smallHero-header-->
 </div><!-- //smallHero-->
 
-<div class="loginContainer">
-	<div class="signUp">
-		<h3>Sign Up</h3>
+<div class="signUpContainer">
+	<div class="signUpDetails">
+		<h3>Create Account</h3>
 		<div class="signUpForm">
 			<form method="post" action="saveContact.php" onsubmit="return validateForm()">
-				<label>Full Name</label>
-				<input type="text" name="strFullName" class="requiredField" id="strFullName">
+				<label>First Name</label>
+				<input type="text" name="strFirstName" class="requiredField" id="strFirstName">
+
+				<label>Last Name</label>
+				<input type="text" name="strLastName" class="requiredField" id="strLastName">
 				
 				<label>Email Address</label>
-				<input type="text" name="strEmailAddress" class="requiredField" id="strEmailAddress">
+				<input type="text" name="strEmail" class="requiredField" id="strEmail">
 
 				<label>Password</label>
 				<input type="text" name="strPassword" class="requiredField" id="strPassword">
 
+				<label>Phone Number</label>
+				<input type="text" name="nPhone" class="requiredField" id="nPhone">
+
+				<label>City</label>
+				<input type="text" name="strCity" class="requiredField" id="strCity">
+
+				<label>Province</label>
+				<input type="text" name="nProvinceID" class="requiredField" id="nProvinceID">
+
 				<input type="submit" value="Sign Up" class="completeBtn"> 
 			</form>
 		</div><!--signUpForm-->	
-	</div><!--signUp-->
-</div><!--loginContainer-->
+	</div><!--signUpDetails-->
+</div><!--signUpContainer-->
